dd7d7d019bf5e0e34e32b8ce3a3bc5142edd294d
tomasfriz <tomyagustinf@gmail.com>
2021-12-29 12:31:15 -0300

    Terminado definitivamente

---
+++ Actividades/Act 1/tipos_datos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tipos_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic num1 = 100;
+            dynamic num2 = 15.50;
+            int num3 = 50;
+            double num4 = 100.50;
+            float num5 = 5.5f;
+            bool estado = true;
+            char letra = 'T';
+            string palabra = "Hola Mundo";
+
+            Console.WriteLine("La variable num1 contiene el número: " + num1);
+            Console.WriteLine("La variable num2 contiene el número: " + num2);
+            Console.WriteLine("La variable num3 contiene el número: " + num3);
+            Console.WriteLine("La variable num4 contiene el número: " + num4);
+            Console.WriteLine("La variable num5 contiene el número: " + num5);
+            Console.WriteLine("La variable estado contiene el estado: " + estado);
+            Console.WriteLine("La variable letra contiene la letra: " + letra);
+            Console.WriteLine("La variable palabra contiene la frase: " + palabra);
+        }
+    }
+}
---
+++ Proyectos del curso/Friz.Tomas/Operadores/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operadores relacionales
+
+            double pesoTest;
+
+            Console.Write("Digita tu peso: ");
+            pesoTest = Convert.ToDouble(Console.ReadLine());
+
+            if (pesoTest <= 100)
+            {
+                Console.WriteLine("Cumple con la condicion 1");
+            }
+            if (pesoTest >= 100)
+            {
+                Console.WriteLine("Cumple con la condicion 2");
+            }
+            if (pesoTest == 100)
+            {
+                Console.WriteLine("Cumple con la condicion 3");
+            }
+            if (pesoTest != 100)
+            {
+                Console.WriteLine("Cumple con la condicion 4");
+            }
+            
+            //------------------------------------------------------------------
+
+            //Operadores logicos
+
+            double peso;
+            byte edad;
+
+            Console.Write("Digita tu peso: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digita tu edad: ");
+            edad = Convert.ToByte(Console.ReadLine());
+
+            Console.Clear();
+
+            if (peso > 100 && edad >= 15)
+            {
+                Console.WriteLine("Tu peso es normal");
+            }
+
+            //if (peso > 100 || edad >= 15)
+            //{
+            //    Console.WriteLine("Tu peso es normal");
+            //}
+        }
+    }
+}
